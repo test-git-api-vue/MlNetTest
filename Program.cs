e9c938bff77d06bd48bd57aaae272e871e2e1d7b
Dmitriy Boyko <dmitriy.boyko@arsis.ru>
2020-07-27 18:06:47 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MlNetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
